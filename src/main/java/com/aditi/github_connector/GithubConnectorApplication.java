fa4feb3e7d2d33dba66dba705ea2d99a16642b4c
aditiedculcate <106309632+the-aditikumari841@users.noreply.github.com>
2026-04-05 18:36:33 +0530

    chore : initial project seetup

---
+++ src/main/java/com/aditi/github_connector/GithubConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.aditi.github_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditi/github_connector/GithubConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditi.github_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
