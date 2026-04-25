0d6605ea1dae30ef7ee692421bd2e6573aef159f
aditiedculcate <106309632+the-aditikumari841@users.noreply.github.com>
2026-04-25 12:53:36 +0530

    Integrate Maven Checkstyle for automated PR analysis and comment formatting

--- src/main/java/com/aditi/github_connector/ci/CIExecutor.java
+++ src/main/java/com/aditi/github_connector/ci/CIExecutor.java
@@ -2,6 +2,10 @@ package com.aditi.github_connector.ci;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 @Component
 public class CIExecutor {
     public String cloneRepo(String repoUrl) {
@@ -12,7 +16,13 @@ public class CIExecutor {
                     "git", "clone", repoUrl, dir
             );
             builder.inheritIO();
-            builder.start().waitFor();
+
+            Process process = builder.start();
+            int exitCode = process.waitFor();
+
+            if (exitCode != 0) {
+                throw new RuntimeException("clone failed with exit code " + exitCode);
+            }
         } catch (Exception e) {
             throw new RuntimeException("Clone failed" + e.getMessage(), e);
         }
@@ -20,6 +30,31 @@ public class CIExecutor {
     }
 
     public String runCheckStyle(String repoPath) {
-        return "Checkstyle passed (dummy result)";
+        try {
+            ProcessBuilder builder = new ProcessBuilder(
+                    "mvn", "checkstyle:check"
+            );
+            builder.directory(new File(repoPath));
+            builder.redirectErrorStream(true);
+
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream())
+            );
+            StringBuilder output = new StringBuilder();
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            int exitCode = process.waitFor();
+            if(exitCode != 0) {
+                output.append("\n (Checkstyle found issues)");
+            }
+            return output.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Checkstyle execution failed: " + e.getMessage(), e);
+        }
     }
 }
